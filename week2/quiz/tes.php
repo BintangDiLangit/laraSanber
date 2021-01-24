39a25917e79d1645e14ca9af681a87b75f49b756
dakocan <dakocan@localhost.localdomain>
2021-01-24 18:54:13 +0700

    quizweek2

---
+++ week2/quiz/tes.php
@@ -0,0 +1,58 @@
+<?php 
+    // $numbers = [75, 80, 60];
+    // $numbers[] = 100;
+    // print_r($numbers);
+
+    // $nama = "abduh";
+    // if (strlen($nama)<=5 && $nama[strlen($nama)-1]== "h") {
+    //     echo "true";
+    // }
+    // echo "<br>";
+    // echo strrev($nama);
+    // echo "<br>";
+    // echo strlen($nama);
+    // echo "<br>";
+    /*echo str_word_count($nama);
+    echo "<br>";
+    $my_var;*/
+
+    // $murid = ["nama" => "Adrian", "alamat" => "Bandung", "gender" => "Laki-laki"];
+    // print_r($murid);
+
+    // function hitung($str){
+    //     $explo = [];
+    //     $hasil = 0;
+    //     $chars = preg_split('//', $str, -1, PREG_SPLIT_NO_EMPTY);
+        
+
+    //     for ($i=0; $i < strlen($str); $i++) { 
+    //         if ($chars[$i] == "*" ) {
+            
+    //             $explo = explode("*",$str);
+    //             $hasil += intval($explo[0] * $explo[1]); 
+    //         }elseif ($chars[$i] == ":") {
+    //             $explo = explode(":",$str);
+    //             $hasil += intval($explo[0] / $explo[1]); 
+    //         }elseif ($chars[$i] == "-") {
+    //             $explo = explode("-",$str);
+    //             $hasil += intval($explo[0] - $explo[1]); 
+    //         }elseif ($chars[$i] == "+") {
+    //             $explo = explode("+",$str);
+    //             $hasil += intval($explo[0] + $explo[1]); 
+    //         }elseif ($chars[$i] == "%") {
+    //             $explo = explode("%",$str);
+    //             $hasil += intval($explo[0] % $explo[1]); 
+    //         }
+    //     }
+        
+    //     echo $hasil;
+    //     echo "<br>";
+    // }
+
+    // echo hitung("102*2");
+    // echo hitung("2+3");
+    // echo hitung("100:25");
+    // echo hitung("10%2");
+    // echo hitung("99-2");
+
+?>
