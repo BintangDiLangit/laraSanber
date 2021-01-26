0303b61e989267c37e60772b8b97a6823c338bb0
BintangDiLangit <syekhuda463@gmail.com>
2021-01-26 22:54:46 +0700

    laravel MVC week 3 day 2 rev1

--- week3/day2/blog/app/Http/Controllers/AuthController.php
+++ week3/day2/blog/app/Http/Controllers/AuthController.php
@@ -13,8 +13,15 @@ class AuthController extends Controller
     public function wlcm(Request $request)
     {
         // dd($request->all());
+
+        // cara 1
         $first = $request["fName"];
         $last = $request["lName"];
         return view('welcome')->with('first', $first)->with('last', $last);
+
+        // cara 2
+        // $first = $request->fName;
+        // $last = $request->lName;
+        // return view('welcome', compact('first', 'last'));
     }
 }
